b50f280ddb2c8b606d08f5931eaf0aa5c33f608f
letu <levantu.13139@gmail.com>
2021-11-18 13:13:08 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './modules/employee/employee.component';
+import { ProjectComponent } from './modules/project/project.component';
+import { TaskComponent } from './modules/task/task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'project', component: ProjectComponent, loadChildren: () => import('./modules/project/project.module').then(m => m.ProjectModule) }, 
+  { path: 'task', component: TaskComponent, loadChildren: () => import('./modules/task/task.module').then(m => m.TaskModule) },
+  { path: 'employee', component: EmployeeComponent, loadChildren: () => import('./modules/employee/employee.module').then(m => m.EmployeeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFolder, faUser, faListAlt } from '@fortawesome/free-regular-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private library: FaIconLibrary){
+    library.addIcons(faFolder, faUser, faListAlt)
+  }
+ }
